b6cb015d140867261781eb6145b549b524daaccd
Jacob Handwerger <jstudyeth@gmail.com>
2023-06-06 19:28:01 -0400

    quick fix

--- src/App.js
+++ src/App.js
@@ -9,47 +9,54 @@ const App = () => {
       avatar: 'avatar-mark-webber.webp',
       action: 'reacted to your recent post',
       other: 'My first tournament today!',
-      time: '1m ago'
+      time: '1m ago',
+      notified: false
     },
     {
       user: 'Angela Gray',
       avatar: 'avatar-angela-gray.webp',
       action: 'followed you',
-      time: '5m ago'
+      time: '5m ago',
+      notified: false
     },
     {
       user: 'Jacob Thompson',
       avatar: 'avatar-jacob-thompson.webp',
       action: 'has joined your group',
       other: 'Chess Club',
-      time: '1 day ago'
+      time: '1 day ago',
+      notified: false
     },
     {
       user: 'Rizky Hasanuddin',
       avatar: 'avatar-rizky-hasanuddin.webp',
       action: 'sent you a private message',
       other: 'Hello, thanks for setting up the Chess Club ...',
-      time: '5 days ago'
+      time: '5 days ago',
+      notified: true
     },
     {
       user: 'Kimberly Smith',
       avatar: 'avatar-kimberly-smith.webp',
       action: 'commented on your picture',
-      time: '1 week ago'
+      time: '1 week ago',
+      notified: true
     },
     {
       user: 'Nathan Peterson',
       avatar: 'avatar-nathan-peterson.webp',
       action: 'reacted to your recent post',
       other: '5 end-game strategies to increase your win rate',
-      time: '2 weeks ago'
+      time: '2 weeks ago',
+      notified: true
     },
     {
       user: 'Anna Kim',
       avatar: 'avatar-anna-kim.webp',
       action: 'left the group',
       other: 'Chess Club',
-      time: '2 weeks ago'
+      time: '2 weeks ago',
+      notified: true
     }
   ]
 
@@ -60,31 +67,22 @@ const App = () => {
           <h2>Notifications <span className="notifications-number">{notifications}</span></h2>
           <button className="mark-all" onClick={()=>setNotifications(0)}>Mark all as read</button>
         </div>
-        {
-        notificationsArr.map((e) => {
-          return (
-            <div className="gray-bg">
-              <img src={e.avatar} alt="avatar" />
-              <h4>{e.user}</h4>
-              <p>{e.action}</p>
-              <p>{e.other}</p>
-            </div>
-          )
-        })
-        }
-          {/* <div className={notifications ? 'gray-bg' : 'notified'}>
-            <img src="avatar-mark-webber.webp" alt="user" />
-            <h3>Mark Webber <span className="second-noti">a</span> <span className="third-noti">a</span></h3>
-            <div className="time"></div>
-          </div>
-          <div className={notifications ? 'gray-bg' : 'notified'}>
-            <img src="avatar-angela-gray.webp" alt="user" />
-            <h3>Angela Gray <span className="second-noti">a</span> <span className="third-noti">a</span></h3>
-          </div>
-          <div className={notifications ? 'gray-bg' : 'notified'}>
-            <img src="avatar-jacob-thompson.webp" alt="user" />
-            <h3>Jacob Thompson <span className="second-noti">a</span> <span className="third-noti-blue">a</span></h3>
-          </div> */}
+        <div className="bottom">
+          {
+          notificationsArr.map((e) => {
+            return (
+              <div className={e.notified ? 'notified' : 'gray-bg'}>
+                <img src={e.avatar} alt="avatar" />
+                <h4>{e.user}</h4>
+                <p>{e.action}</p>
+                <p>{e.other}</p>
+                <p>{e.time}</p>
+
+              </div>
+            )
+          })
+          }
+        </div>
       </div>
     </>
   );
